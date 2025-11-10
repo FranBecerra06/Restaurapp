7018c0b0e621cb2195347e03ccf1b9027d9bbdee
FranBecerra06 <fbecerragarcia1704@gmail.com>
2025-11-10 13:04:09 +0100

    Commit estructura vacía CRUD alérgenos
    Una estructura vacía de cada método del CRUD de los alérgenos.

--- src/main/java/DAO/AlergenoDAO.java
+++ src/main/java/DAO/AlergenoDAO.java
@@ -1,4 +1,36 @@
 package DAO;
 
 public class AlergenoDAO {
+	
+	//CRUD DE ALÉRGENOS
+	
+	//-- INSERTAR ALÉRGENO --
+	
+	
+	
+	
+	
+	
+	 
+	//-- MODIFICAR ALÉRGENO --
+	
+	
+	
+	
+	
+	
+	
+	//-- ELIMINAR ALÉRGENO --
+	
+	
+	
+	
+	
+	
+	
+	//-- LISTAR ALÉRGENOS --
+	
+	
+	
+	
 }
