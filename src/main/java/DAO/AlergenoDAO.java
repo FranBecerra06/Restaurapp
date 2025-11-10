328453bf78fa6621a50598a9072d458056270a93
FranBecerra06 <fbecerragarcia1704@gmail.com>
2025-11-10 14:11:55 +0100

    Actualizaciones

--- src/main/java/DAO/AlergenoDAO.java
+++ src/main/java/DAO/AlergenoDAO.java
@@ -1,23 +1,34 @@
 package DAO;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
+import com.mysql.cj.xdevapi.Result;
+
+import ConexionBd.ConexionBD;
+import DTO.AlergenoDTO;
+
 public class AlergenoDAO {
-	
-	//CRUD DE ALÉRGENOS
-	
-	//-- INSERTAR ALÉRGENO --
-	
-	public Alergeno insertarAlergeno(Alergeno a) throws SQLException{
+
+	// CRUD DE ALÉRGENOS
+
+	// -- INSERTAR ALÉRGENO --
+
+	public AlergenoDTO insertarAlergeno(AlergenoDTO a) throws SQLException{
 		
 		String sql = "INSERT INTO alergeno (id_alergeno, nombre) VALUES(?,?)";
-		PreparedStatement pst = conn = PreparedStatement(sql, Statement.RETURN_GENERATED_KEYS)){
-			
+		
+		try(Connection conn = ConexionBD.getConnection();
+		PreparedStatement pst = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)){
+
 			pst.setInt(1, a.getid_alergeno());
 			pst.setString(2, a.getnombre());
 			pst.executeUpdate();
 			
-			try(Result.Set rs = pst.getGeneratedKeys(){
+			try(ResultSet rs = pst.getGeneratedKeys(){
 				if(rs.next()) {
 					
 					a.setId(rs.getInt(1));
@@ -33,34 +44,19 @@ public class AlergenoDAO {
 		
 		
 	}
-	
-	
-	
-	
-	 
-	//-- MODIFICAR ALÉRGENO --
-	
-	
-	
-	
-	
-	
-	
-	//-- ELIMINAR ALÉRGENO --
-	
-	
-	
-	
-	
-	
-	
-	//-- LISTAR ALÉRGENOS --
-	
-	
-	
-	
-<<<<<<< Updated upstream
+
+// -- MODIFICAR ALÉRGENO --
+
+	public AlergenoDTO modificarAlergeno (AlergenoDTO a) throws SQLException{
+		
+		String sql = "UPDATE alergeno SET nombre = ? WHERE id_alergeno = ?";
+		try (Connection conn = Conexion
+		
+		
+	}
+
+// -- ELIMINAR ALÉRGENO --
+
+// -- LISTAR ALÉRGENOS --
+
 }
-=======
-}
->>>>>>> Stashed changes
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,11 @@ module pack.restaurantegestion {
     requires javafx.controls;
     requires javafx.fxml;
 
+	requires java.sql;
+
+	requires mysql.connector.j;
+
+
 
     opens pack.restaurantegestion to javafx.fxml;
     exports pack.restaurantegestion;
