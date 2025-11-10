a5226ac5ba02fe0880a1e3204508bc55a595e222
FranBecerra06 <fbecerragarcia1704@gmail.com>
2025-11-10 13:51:22 +0100

    Método insertar alérgeno creado

--- src/main/java/DAO/AlergenoDAO.java
+++ src/main/java/DAO/AlergenoDAO.java
@@ -1,12 +1,38 @@
 package DAO;
 
+import java.sql.Statement;
+
 public class AlergenoDAO {
 	
 	//CRUD DE ALÉRGENOS
 	
 	//-- INSERTAR ALÉRGENO --
 	
-	
+	public Alergeno insertarAlergeno(Alergeno a) throws SQLException{
+		
+		String sql = "INSERT INTO alergeno (id_alergeno, nombre) VALUES(?,?)";
+		PreparedStatement pst = conn = PreparedStatement(sql, Statement.RETURN_GENERATED_KEYS)){
+			
+			pst.setInt(1, a.getid_alergeno());
+			pst.setString(2, a.getnombre());
+			pst.executeUpdate();
+			
+			try(Result.Set rs = pst.getGeneratedKeys(){
+				if(rs.next()) {
+					
+					a.setId(rs.getInt(1));
+				}
+				
+				
+				
+			}
+			
+			
+		}
+		return a;
+		
+		
+	}
 	
 	
 	
