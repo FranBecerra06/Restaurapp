f048bc4d6227c1af5314c7510de519ef37c4bac8
FranBecerra06 <fbecerragarcia1704@gmail.com>
2025-11-11 12:08:24 +0100

    AlergenoDAO Finalizado

--- src/main/java/DAO/AlergenoDAO.java
+++ src/main/java/DAO/AlergenoDAO.java
@@ -5,6 +5,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.mysql.cj.xdevapi.Result;
 
@@ -17,44 +19,37 @@ public class AlergenoDAO {
 
 	// -- INSERTAR ALÉRGENO --
 
-	public AlergenoDTO insertarAlergeno(AlergenoDTO a) throws SQLException{
-		
-		String sql = "INSERT INTO alergeno (id_alergeno, nombre) VALUES(?,?)";
-		
-		try(Connection conn = ConexionBD.getConnection();
-		PreparedStatement pst = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)){
+	public AlergenoDTO crearAlergeno(AlergenoDTO a) throws SQLException {
 
-			pst.setInt(1, a.getid_alergeno());
-			pst.setString(2, a.getnombre());
+		String sql = "INSERT INTO alergeno (nombre) VALUES(?)";
+
+		try (Connection conn = ConexionBD.getConnection();
+				PreparedStatement pst = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+
+			pst.setString(1, a.getNombre());
 			pst.executeUpdate();
-			
-			try(ResultSet rs = pst.getGeneratedKeys(){
-				if(rs.next()) {
-					
+
+			try (ResultSet rs = pst.getGeneratedKeys()) {
+				if (rs.next()) {
+
 					a.setId(rs.getInt(1));
 				}
-				
-				
-				
+
 			}
-			
-			
+
 		}
 		return a;
-		
-		
+
 	}
 
 // -- MODIFICAR ALÉRGENO --
 
 	public boolean modificarAlergeno(AlergenoDTO a) throws SQLException {
 
-		String sql = "UPDATE alergeno SET nombre = ? WHERE id_alergeno = ?";
-		try (Connection conn = ConexionBD.getConnection();
-				PreparedStatement pst = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+		String sql = "UPDATE alergeno SET nombre = ? WHERE nombre = ?";
+		try (Connection conn = ConexionBD.getConnection(); PreparedStatement pst = conn.prepareStatement(sql)) {
 
-			pst.setId(1, a.getid_alergeno());
-			pst.setString(2, a.getnombre());
+			pst.setString(1, a.getNombre());
 			int filas = pst.executeUpdate();
 			return filas > 0;
 
@@ -63,6 +58,39 @@ public class AlergenoDAO {
 
 // -- ELIMINAR ALÉRGENO --
 
+	public boolean eliminarAlergeno(int id) throws SQLException {
+
+		String sql = "DELETE FROM alergeno WHERE nombre = ?";
+
+		try (Connection conn = ConexionBD.getConnection(); PreparedStatement pst = conn.prepareStatement(sql)) {
+
+			pst.setInt(1, id);
+			int filas = pst.executeUpdate();
+			return filas > 0;
+
+		}
+	}
+
 // -- LISTAR ALÉRGENOS --
+
+	public List<AlergenoDTO> listarAlergenos() throws SQLException {
+
+		List<AlergenoDTO> lista = new ArrayList<>();
+		String sql = "SELECT nombre FROM alergeno";
+		try (Connection conn = ConexionBD.getConnection();
+
+				Statement st = conn.createStatement();
+				ResultSet rs = st.executeQuery(sql)) {
+
+			while (rs.next()) {
+
+				lista.add(new AlergenoDTO(rs.getString("nombre")));
+
+			}
+
+		}
+		return lista;
+
+	}
 
 }
