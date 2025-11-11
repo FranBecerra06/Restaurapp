7597e70d60ed300fa271f45986ee06a3d394cbf8
FranBecerra06 <fbecerragarcia1704@gmail.com>
2025-11-11 12:27:12 +0100

    Update AlergenoDAO

--- src/main/java/DAO/AlergenoDAO.java
+++ src/main/java/DAO/AlergenoDAO.java
@@ -1,14 +1,12 @@
 package DAO;
 
-import java.sql.Connection;
+import java.sql.Connection;	
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-
-import com.mysql.cj.xdevapi.Result;
 
 import ConexionBd.ConexionBD;
 import DTO.AlergenoDTO;
