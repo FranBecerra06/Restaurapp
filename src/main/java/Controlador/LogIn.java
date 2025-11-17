5edd7a888dc049d013e31f6ac50a5eb59a327b45
antonioroman97 <antonioroman97@gmail.com>
2025-11-17 13:14:24 +0100

    LogIn Funciona con base de datos

--- src/main/java/Controlador/LogIn.java
+++ src/main/java/Controlador/LogIn.java
@@ -1,12 +1,24 @@
 package Controlador;
 
+import ConexionBd.ConexionBD;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public class LogIn {
+
+    private List<String> nombresUsuario = new ArrayList<>();
+
 
     @FXML
     private Button loginBtn;
@@ -14,18 +26,145 @@ public class LogIn {
     private TextField usuarioTF;
     @FXML
     private PasswordField contraUsuPF;
+    @FXML
+    private Label textoAuxiliar;
 
-    public void logInAction(ActionEvent event){
-        if(usuarioTF.getText().equals("Admin") && contraUsuPF.getText().equals("1234")){
+
+    @FXML
+    private void initialize() {//IMPORTANTE! este metodo ocurre al iniciarse la aplicacion
+        loginBtn.disableProperty().bind(//Enlazamos la propiedad isDisabled del boton a la propiedad isEmpty del passwordField
+                contraUsuPF.textProperty().isEmpty()
+        );
+    }
+
+    //Metoto principal, ocurre al clicar en iniciar sesion
+    public void logInAction(ActionEvent event) {
+        getUsuarios();
+        boolean busquedaUsuarioExitosa = false;
+        String nombreUsuario="";
+        boolean casoAdmin = false;
+
+        textoAuxiliar.setText("");
+
+        //Control de caso de Admin
+        if (usuarioTF.getText().equals("Admin") ) {
+            casoAdmin = true;
+            if(contraUsuPF.getText().equals("1234")){
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                alert.setTitle("Admin");
+                alert.setContentText("Admin ha iniciado sesión");
+                Optional<ButtonType> result = alert.showAndWait();
+            }else{
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Admin");
+                alert.setContentText("Contraseña incorrecta");
+                Optional<ButtonType> result = alert.showAndWait();
+            }
+
+        }
+
+        //Control de caso de usuario existente
+        while(!busquedaUsuarioExitosa){
+            for(String nombreEnBusqueda : nombresUsuario){
+                if(usuarioTF.getText().equals(nombreEnBusqueda)){
+                    nombreUsuario = nombreEnBusqueda;
+                    busquedaUsuarioExitosa = true;
+                }
+            }
+            break;
+        }
+        if(busquedaUsuarioExitosa)
+            iniciarSesionUsuario(getContrasena(nombreUsuario),nombreUsuario);
+        //Control de caso de usuario inexistente
+        if(!busquedaUsuarioExitosa)
+            if(casoAdmin){
+                textoAuxiliar.setText("Contraseña inválida para Adminitrador");
+            }else {
+                textoAuxiliar.setText("Usuario no reconocido");
+
+            }
+    }
+
+
+    //Este metodo devuelve todos los usuarios almacenados en la base de datos
+    public void getUsuarios(){
+        try{
+            Connection conn = ConexionBD.getConnection();
+            System.out.println("Conexión establecida");
+            Statement usersRetrieve = conn.createStatement();
+            //Query SQL que se hará y se guarda como ResultSet
+            String stm1 = "select nombre_usuario from usuario";
+            ResultSet rs = usersRetrieve.executeQuery(stm1);
+            //Aqui guardo los nombres recuperados en el resultSet en un arraylist nombresUsuario
+            while(rs.next()){
+                nombresUsuario.add(rs.getString("nombre_usuario"));
+            }
+        } catch (SQLException e) {
+            System.out.println("Problema obteniendo usuarios");
+            e.printStackTrace();
+        }
+
+    }
+
+    public String getContrasena(String nombre){
+        String contra = "";
+        try {
+            Connection connection = ConexionBD.getConnection();
+            Statement statement = connection.createStatement();
+            String query = "select contrasena from usuario where nombre_usuario = '"+nombre+"'";
+            ResultSet rs = statement.executeQuery(query);
+            while (rs.next()){
+                contra = rs.getString("contrasena");
+            }
+        } catch (SQLException e) {
+            System.out.println("Error en getContrasena");;
+        }
+        return contra;
+    }
+
+    public void iniciarSesionUsuario(String contrasena,String nombreUsuario){
+        if(contraUsuPF.getText().equals(contrasena)){
+            System.out.println("Usuario "+nombreUsuario+" reconocido");
             Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-            alert.setTitle("Admin");
-            alert.setContentText("Admin ha iniciado sesión");
+            alert.setTitle("Usuario");
+            alert.setContentText("Usuario " +nombreUsuario+" inició sesion");
             Optional<ButtonType> result = alert.showAndWait();
+            cambiarAClienteLogged();
         }else{
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("No Admin");
-            alert.setContentText("Admin no ha iniciado sesión");
-            Optional<ButtonType> result = alert.showAndWait();
+            textoAuxiliar.setText("Contraseña incorrecta");
+        }
+
+    }
+
+
+
+    @FXML
+    public void cambiarAClienteLogged(){
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/pack/restaurantegestion/clienteLogged.fxml"));
+            Parent root = loader.load();
+            Stage currentScene = (Stage) loginBtn.getScene().getWindow();
+            Scene scene = new Scene(root);
+            currentScene.setScene(scene);
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @FXML
+    public void cambiarASignIn(ActionEvent event){
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/pack/restaurantegestion/SignIn.fxml"));
+            Parent root = loader.load();
+            Stage currentScene = (Stage) loginBtn.getScene().getWindow();
+            Scene scene = new Scene(root);
+            currentScene.setScene(scene);
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
--- src/main/java/pack/restaurantegestion/Main.java
+++ src/main/java/pack/restaurantegestion/Main.java
@@ -1,11 +1,14 @@
 package pack.restaurantegestion;
 
+import ConexionBd.ConexionBD;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class Main extends Application {
 
@@ -15,9 +18,18 @@ public class Main extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+
+
+
+
     }
 
-    public static void main(String[] args) {
-        launch();
-    }
+    /*public static void main(String[] args) {
+
+        try (Connection conn = ConexionBD.getConnection()) {
+            System.out.println("Conexión establecida");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }*/
 }
